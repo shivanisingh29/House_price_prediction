8e88842595edb1b1701ba21147a6d81b2618336a
shivanisingh29 <shivanisinghchauhan29@gmail.com>
2026-05-03 22:28:12 +0530

    Update House_price_prediction.py

--- House_price_prediction.py
+++ House_price_prediction.py
@@ -7,7 +7,8 @@ from sklearn.metrics import mean_squared_error,r2_score
 from sklearn.pipeline import Pipeline
 
 # Data preprocessing
-data=pd.read_csv("Housing.csv")
+load=st.file.uploader("Upload your data",type=['csv'])
+data=pd.read_csv(load)
 print("Dataset shape after reduction: ",data.shape)
 print(data.info())
 print("Duplicate rows: ",data.duplicated().sum())
@@ -46,8 +47,26 @@ y_pred=pipeline.predict(X_test)
 # evaluation
 mse=mean_squared_error(y_test,y_pred)
 r2=r2_score(y_test,y_pred)
-
+accuracy=r2*100
+print(f"Accuracy:{accuracy:.2f}%")
 print("Mean Squared Error:",mse)
 print("R2 Score:",r2)
+
+# UI
+st.metric("r2 Score (Accuracy)",f"{r2:.2f}%")
+st.metric("Mean Squared Error",f"{mse:.2f}%")
+
+# user input
+st.subheader("Predict your house price")
+
+# input for every feature
+input_data={}
+for col in X.columns:
+    input_data[col]=st.number_input(col,value=0)
+
+if st.button("Predict"):
+    input_df=pd.DataFrame([input_data])
+    prediction=pipeline.predict(input_df)
+    st.success(f"Your house price is :{prediction[0]:,.0f}")
 
 
